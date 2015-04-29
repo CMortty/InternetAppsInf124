239356ffb90d4261436b56431f78c3d9d25a3e7f
Vishaal Lalchand <vlalchan@uci.edu>
2015-04-29 00:38:43 -0700

    should finalizes things that are due tomorrow

--- scripts/product.js
+++ scripts/product.js
@@ -1,12 +1,28 @@
-var orderForm = document.getElementById("orderForm");
-var numIphone = orderForm.numIphone.value;
-var numNokia = orderForm.numNokia.value;
-var numToy=orderForm.numToy.value;
-var numAbacus=orderForm.numAbacus.value;
-var numWriter=orderForm.numWriter.value;
 
-function processTotals(){
-  var Total=(numIphone*500)
-  alert("Your Total Is:" +Total);
+
+function ComputeTotal(){
+  var orderForm = document.getElementById("orderForm");
+  var numIphone = orderForm.numIphone.value;
+
+  var numNokia = orderForm.numNokia.value;
+  var numToy=orderForm.numToy.value;
+  var numAbacus=orderForm.numAbacus.value;
+  var numWriter=orderForm.numWriter.value;
+  var Total=(numIphone*500)+(numNokia*499)+(numToy*399)+(numAbacus*200)+(numWriter*48);
+  alert("Your order Total is: "+Total);
 
   }
+
+
+
+function ProcessOrder(){
+  var orderForm = document.getElementById("orderForm");
+  var numIphone = orderForm.numIphone.value;
+  var numNokia = orderForm.numNokia.value;
+  var numToy=orderForm.numToy.value;
+  var numAbacus=orderForm.numAbacus.value;
+  var numWriter=orderForm.numWriter.value;
+  var Total=(numIphone*500)+(numNokia*499)+(numToy*399)+(numAbacus*200)+(numWriter*48);
+  alert("Your order Total is: "+Total+", and it has been added to your cart. Thank You");
+
+    }
