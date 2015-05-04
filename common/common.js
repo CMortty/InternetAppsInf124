12a0ace2d19f313a7cd88a350da6efa04623f6a1
Cory Mortimer <cmm0286@gmail.com>
2015-05-03 17:20:26 -0700

    Fix some things and add drop down menu

--- common/common.js
+++ common/common.js
@@ -2,3 +2,15 @@ $(function () {
     $("#mainMenu").load("common/mainmenu.html");
     $("#logo").load("common/logo.html");
 });
+
+var isShowing = false;
+var about = document.getElementById("about");
+function showAbout() {
+    about = document.getElementById("about");
+    about.style.visibility = 'visible';
+}
+
+function hide() {
+    about = document.getElementById("about");
+    about.style.visibility = "hidden";
+}
