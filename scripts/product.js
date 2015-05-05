976ad29d58d372ca7b4a10713f498075809a25a7
Vishaal Lalchand <vlalchan@uci.edu>
2015-05-04 23:09:14 -0700

    errors in product.js found but not solved

--- scripts/product.js
+++ scripts/product.js
@@ -1,9 +1,5 @@
-
-
-
-
-
-function ProcessOrder(){
+function ProcessOrder() {
+  // alert("We got to the process Order");
   var orderForm = document.getElementById("orderForm");
   var numIphone = orderForm.numIphone.value;
   var numNokia = orderForm.numNokia.value;
@@ -11,14 +7,17 @@ function ProcessOrder(){
   var numAbacus=orderForm.numAbacus.value;
   var numWriter=orderForm.numWriter.value;
   var Total=(numIphone*500)+(numNokia*499)+(numToy*399)+(numAbacus*200)+(numWriter*48);
-  var fname=orderForm.firstName.value;
+  var fName = orderForm.firstName.value;
+
   var lName=orderForm.lastName.value;
   var email=orderForm.email.value;
-  var zip=orderForm.zipCode.value;
+  var zipCode=orderForm.zipCode.value;
+
   if (!validateName(fName)) {
       alert("Invalid first name.");
       return false;
   }
+
   if (!validateName(lName)) {
       alert("Invalid last name.");
       return false;
@@ -28,19 +27,20 @@ function ProcessOrder(){
       alert("Invalid email.");
       return false;
   }
-  if (!validateZip(zip)){
+  //either this call or the method itself is not working
+  if (!validateZip(zipCode)){
     alert("Invalid zip."):
     return false;
   }
   alert("Your order Total is: "+Total+", and it has been added to your cart. Thank You");
 
 
+}
 
-
-
-function validateZip(zip){
+//this method is being screwy ask cory how search works
+function validateZip(zip) {
+  alert("This is where all hell breaks lose")
   var regZip=/^[0-9]{5}(?:-[0-9]{4})?$/;
   return regZip.test(zip);
 
-}
 }
